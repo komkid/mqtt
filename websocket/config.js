1e419bc9664bf9265dba2469536b93e1be3504fc
komkid <komkid@gmail.com>
2016-12-13 16:30:18 +0700

    back to origin

--- websocket/config.js
+++ websocket/config.js
@@ -1,13 +1,13 @@
 // host = '172.16.153.122';	// hostname or IP address
 //host = '127.0.0.1';	// hostname or IP address
-host = '192.168.2.4';	// hostname or IP address
+host = '101.108.133.158';	// hostname or IP address
 port = 1884;
 topic = '#';		// topic to subscribe to
 useTLS = false;
-username = null;
-password = null;
-// username = "jjolie";
-// password = "aa";
+//username = null;
+//password = null;
+ username = "myu";
+ password = "myp";
 
 // path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
 //    defaults to "/mqtt"
