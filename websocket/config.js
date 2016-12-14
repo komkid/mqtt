d348326ad481813244d4c8128a680e15e688429d
komkid <komkid@gmail.com>
2016-12-14 15:49:54 +0700

    Update config.js

--- websocket/config.js
+++ websocket/config.js
@@ -1,6 +1,4 @@
-// host = '172.16.153.122';	// hostname or IP address
-//host = '127.0.0.1';	// hostname or IP address
-host = '101.108.133.158';	// hostname or IP address
+host = '127.0.0.1';	// hostname or IP address
 port = 1884;
 topic = '#';		// topic to subscribe to
 useTLS = false;
