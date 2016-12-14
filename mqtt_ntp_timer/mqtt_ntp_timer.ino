cda692b563fdd72f004f0c2444ea136b1ec49893
komkid <komkid@gmail.com>
2016-12-14 15:55:49 +0700

    Add files via upload

---
+++ mqtt_ntp_timer/mqtt_ntp_timer.ino
@@ -0,0 +1,219 @@
+/*
+ Basic ESP8266 MQTT example
+
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <time.h>
+int timezone = 7 * 3600;                    
+int dst = 0;                                
+struct tm* p_tm;
+
+#include "Timer.h"
+Timer t;
+int timeEvent;
+int mqttEvent;
+
+int updateInterval = 5000;//millisec.
+unsigned int h;
+unsigned int m;
+unsigned int s;
+
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "SCI_EMP";
+const char* password = "619513b917";
+const char* mqtt_server = "192.168.2.4";
+#define LEDPIN LED_BUILTIN
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void blinking(int qty=10){
+  int n;
+  for (n = 1; n < qty ; n++){
+    digitalWrite(LEDPIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    delay(200);                      // Wait for a second
+    digitalWrite(LEDPIN, HIGH);  // Turn the LED off by making the voltage HIGH
+    delay(200);                      // Wait for two seconds (to demonstrate the active low LED)
+  }
+}
+
+void setup() {
+  pinMode(LEDPIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  mqttEvent = t.every(1000, mqtt_job);  
+
+  configTime(timezone, dst, "pool.ntp.org", "time.nist.gov");     //ดึงเวลาจาก Server
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println();
+  time_t now = time(nullptr);
+  p_tm = localtime(&now);
+  h = p_tm->tm_hour;
+  m = p_tm->tm_min;
+  s = p_tm->tm_sec;
+
+  timeEvent = t.every(updateInterval, tikTok);  
+
+  printTimeNow();
+  Serial.println("Now");
+  blinking(20);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  printTimeNow();
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+/*
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+*/  
+  payload[length] = '\0';
+  String strPayload = String((char*)payload);
+  Serial.print(strPayload);
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(LEDPIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(LEDPIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void mqtt_job(){
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    //++value;
+    //snprintf (msg, 75, "hello world #%ld", value);
+    //Serial.print("Publish message: ");
+    //Serial.println(msg);
+    //client.publish("outTopic", msg);
+  }  
+}
+
+void loop() {
+  t.update();
+}
+
+void tikTok(){
+  s = s + (updateInterval / 1000);
+  if((s / 60) > 0) {
+    s = s % 60; 
+    m++;
+    if(m == 60){
+      m = 0;
+      h++;
+      if(h == 24) h = 0;
+    }
+  }
+  //printTimeNow();
+  if(h == 0 & m == 0){
+    delay(60000);
+    ESP.restart();
+  }
+}
+
+void printTimeNow(){
+  Serial.println();
+  Serial.print(h); // print the hour (86400 equals secs per day)
+  Serial.print(':');
+  if ( m < 10 ) {
+    // In the first 10 minutes of each hour, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.print(m); // print the minute (3600 equals secs per minute)
+  Serial.print(':');
+  if ( s < 10 ) {
+    // In the first 10 seconds of each minute, we'll want a leading '0'
+    Serial.print('0');
+  }
+  Serial.print(s); // print the second
+  Serial.print(" >> "); // print the second
+}
+
