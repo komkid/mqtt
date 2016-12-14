845aa8c44fc5187091bb81d2d60e31c64faaa29c
komkid <komkid@gmail.com>
2016-12-14 15:59:09 +0700

    Update mqtt_ntp_timer.ino

--- mqtt_ntp_timer/mqtt_ntp_timer.ino
+++ mqtt_ntp_timer/mqtt_ntp_timer.ino
@@ -43,8 +43,8 @@ unsigned int s;
 
 // Update these with values suitable for your network.
 
-const char* ssid = "SCI_EMP";
-const char* password = "619513b917";
+const char* ssid = "...";
+const char* password = "...";
 const char* mqtt_server = "192.168.2.4";
 #define LEDPIN LED_BUILTIN
 
